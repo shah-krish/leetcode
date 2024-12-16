8f5cc6fea617d59d66f22a4e34bc6bdab409e7fc
krish <krishshah1411@gmail.com>
2024-12-16 12:18:32 -0500

    leetcode 18

---
+++ striver_a2z/src/FourSum.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class FourSum {
+    public static void main(String[] args) {
+
+    }
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        ArrayList<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i = 0; i<nums.length-3;i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            for(int j = i+1; j<nums.length-2;j++){
+                if (j > i + 1 && nums[j - 1] == nums[j])
+                    continue;
+
+                int k = j+1;
+                int l = nums.length-1;
+
+                while(k<l){
+                    int sum = nums[i] + nums[j];
+                    sum+=nums[k] + nums[l];
+
+                    if(sum == target){
+                        list.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                        while (k < l && nums[k - 1] == nums[k])
+                            k++;
+                        while (k < l && nums[l + 1] == nums[l])
+                            l--;
+                    }
+                    else if(sum<target){
+                        k++;
+                    }
+                    else{
+                        l--;
+                    }
+                }
+
+            }
+        }
+        return list;
+    }
+}
