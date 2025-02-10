2fa9b81f94a7d5a6f4333ba93885f7f12689130b
krish <krishshah1411@gmail.com>
2025-02-09 22:42:58 -0500

    floorCeil

---
+++ striver_a2z/src/floorCeil.java
@@ -0,0 +1,47 @@
+import java.util.Arrays;
+
+//https://www.naukri.com/code360/problems/ceiling-in-a-sorted-array_1825401
+
+
+public class floorCeil {
+    public static void main(String[] args) {
+        int[] a = {3, 4, 7, 8, 8, 10};
+        System.out.println(getFloorAndCeil(a,a.length,5));
+    }
+    static int findFloor(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] <= x) {
+                ans = arr[mid];
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    static int findCeil(int[] arr, int n, int x) {
+        int low = 0, high = n - 1;
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] >= x) {
+                ans = arr[mid];
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static int[] getFloorAndCeil(int[] arr, int n, int x) {
+        int f = findFloor(arr, n, x);
+        int c = findCeil(arr, n, x);
+        return new int[] {f, c};
+    }
+}
