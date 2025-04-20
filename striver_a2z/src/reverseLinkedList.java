7bb251f145b2273cedbf5c50b16df4c5858c934f
shah-krish <krishshah1411@gmail.com>
2025-04-20 16:16:19 -0400

    leetcode 206

--- striver_a2z/src/reverseLinkedList.java
+++ striver_a2z/src/reverseLinkedList.java
@@ -23,15 +23,20 @@ public class reverseLinkedList {
             next = next1;
         }
     }
-    public ListNode reverseList(ListNode head) {
-        ListNode prev=null;
-        ListNode curr=head;
-        while(curr!=null){
-            ListNode  temp=curr.next;
-            curr.next=prev;
-            prev = curr;
-            curr=temp;
+    public ListNode reverseList(ListNode head){
+        if(head==null||head.next==null){
+            return head;
         }
+        ListNode prev = head;
+        ListNode curr = head.next;
+        while(curr!=null){
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+
+        head.next = null;
         return prev;
     }
 
