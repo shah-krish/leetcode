fd077036cda4cf56ed9c5c02f790e48a01403569
shah-krish <krishshah1411@gmail.com>
2025-05-06 23:06:11 -0400

    leetcode 169

--- striver_a2z/src/majorityElement.java
+++ striver_a2z/src/majorityElement.java
@@ -4,22 +4,23 @@ public class majorityElement {
     public static void main(String[] args) {
 
     }
-    public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i =0 ; i<nums.length;i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i],1);
+
+        public int majorityElement(int[] nums) {
+            int counter = 0;
+            int m = 0;
+            for(int i : nums){
+                if(counter==0){
+                    m = i;
+                    counter = 1;
+                }
+                else if(m==i){
+                    counter++;
+                }
+                else{
+                    counter--;
+                }
             }
-            else{
-                map.put(nums[i],map.get(nums[i])+1);
-            }
+            return m;
         }
-        double minFreq = Math.floor(nums.length/2);
-        for(int i =0; i<nums.length;i++){
-            if(map.get(nums[i])>minFreq){
-                return nums[i];
-            }
-        }
-        return 0;
-    }
+
 }
