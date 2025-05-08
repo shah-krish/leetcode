ad3abda0e9881f57d8e2c0b5e598c45404451ee1
shah-krish <krishshah1411@gmail.com>
2025-05-07 22:13:17 -0400

    leetcode 121

--- striver_a2z/src/bestTimeToBuySellStock.java
+++ striver_a2z/src/bestTimeToBuySellStock.java
@@ -12,3 +12,15 @@ public class bestTimeToBuySellStock {
         return sell;
     }
 }
+/* solved may 7 '25
+ public int maxProfit(int[] prices) {
+        int max = 0;
+        int buyPrice = Integer.MAX_VALUE;
+        int sellPrice = 0;
+        for(int i =0; i< prices.length-1; i++){
+            buyPrice = Math.min(buyPrice, prices[i]);
+            max = Math.max(max, prices[i+1] - buyPrice);
+        }
+        return max;
+    }
+ */
