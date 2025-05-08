c44c689ca5566bfcf6b0e750f554d241ae83561e
shah-krish <krishshah1411@gmail.com>
2025-05-07 22:10:36 -0400

    leetcode 53

--- striver_a2z/src/maximumSubarray.java
+++ striver_a2z/src/maximumSubarray.java
@@ -15,3 +15,23 @@ public class maximumSubarray {
         return sum;
     }
 }
+/* Solved on may 7 '25
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int ans = Integer.MIN_VALUE;
+        if(nums.length==1){
+            return nums[0];
+        }
+        for(int i = 0; i<nums.length; i++){
+            if(sum<0){
+             sum = 0;
+            }
+            sum+=nums[i];
+            ans = Math.max(sum,ans);
+
+        }
+        return ans;
+    }
+}
+ */
