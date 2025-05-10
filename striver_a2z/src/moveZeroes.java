78482e6827248c24a2b9cefdcf272a152febda3d
shah-krish <krishshah1411@gmail.com>
2025-05-09 22:00:46 -0400

    leetcode 283

--- striver_a2z/src/moveZeroes.java
+++ striver_a2z/src/moveZeroes.java
@@ -3,6 +3,28 @@ public class moveZeroes{
 
     }
     public void moveZeroes(int[] nums) {
-        
+        int zeroPointer = 0;
+        int numberPointer = 0;
+        while(zeroPointer< nums.length && numberPointer<nums.length){
+            if(nums[zeroPointer]!=0){
+                zeroPointer++;
+            }
+            else if(nums[numberPointer]==0){
+                numberPointer++;
+            }
+            else if(zeroPointer<numberPointer){
+                swap(nums, zeroPointer, numberPointer);
+                zeroPointer++;
+                numberPointer++;
+            }
+            else{
+                numberPointer++;
+            }
+        }
+    }
+    public void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
