56c7adadccb4350171cdb823ebfbb6f02cc99cc9
shah-krish <krishshah1411@gmail.com>
2025-05-13 22:22:34 -0400

    leetcode 560

--- striver_a2z/src/subArraySumEqualsK.java
+++ striver_a2z/src/subArraySumEqualsK.java
@@ -1,47 +1,29 @@
+import java.util.HashMap;
+
 public class subArraySumEqualsK {
     public static void main(String[] args) {
         int[] arr = {1, 2,1,2,1};
         System.out.println(subarraySum(arr, 3));
     }
     public static int subarraySum(int[] nums, int k) {
-        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap();
         int counter = 0;
-        for (int i =0; i<nums.length; i++){
-            sum += nums[i];
-            if(sum == k && (i+1 < nums.length) && (nums[i+1]!=0)){
-                counter ++;
-                sum = 0;
-            }
-            else if(sum == k && (i+1 < nums.length) && (nums[i+1]==0)){
-                counter ++;
-            }
-          //  System.out.println("sum = "+sum);
-        //    System.out.println("counter = "+counter);
-        }
-        return counter;
-    }
-}
-    /* BRUTE FORCE APPROACH SHITTY TIME COMPLEXITY
-    public static int subarraySum(int[] nums, int k) {
-        int counter = 0;
-        int sum;
+        int accumulator = 0;
+        map.put(0, 1);
         for(int i =0; i<nums.length; i++){
-            sum =0;
-            for(int j = i; j<nums.length;j++){
-                //System.out.println("i ="+i + "j="+j);
-                sum+=nums[j];
-                if(sum == k){
-                    counter ++;
-                }
-
-
-
-              //  System.out.println(sum);
-              //  System.out.println("counter = " + counter);
+            accumulator += nums[i];
+            if(map.containsKey(accumulator-k)){
+                counter+=map.get(accumulator-k);
+            }
+            if(map.containsKey(accumulator)){
+                map.put(accumulator, map.get(accumulator)+1);
+            }
+            else{
+                map.put(accumulator, 1);
             }
         }
+        //System.out.println(map);
         return counter;
     }
-}
+    }
 
-     */
