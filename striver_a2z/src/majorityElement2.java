b0e49dd8ed723b699294e7472066035b2d7bae94
shah-krish <krishshah1411@gmail.com>
2025-05-17 11:10:49 -0400

    leetcode 229

--- striver_a2z/src/majorityElement2.java
+++ striver_a2z/src/majorityElement2.java
@@ -2,25 +2,58 @@ import java.util.*;
 public class majorityElement2 {
     public static void main(String[] args) {
 
-    }
+        }
+
+
+
     public List<Integer> majorityElement(int[] nums) {
-        double limit  = Math.floor(nums.length/3);
-        List<Integer> list = new ArrayList<>();
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i : nums){
-            if(!map.containsKey(i)){
-                map.put(i,1);
+        ArrayList<Integer> list = new ArrayList();
+        int number1 = 0;
+        int counter1 = 0;
+        int number2 = 0;
+        int counter2=0;
+        int limit = nums.length/3;
+
+        for(int i = 0; i<nums.length;i++){
+            if(nums[i] == number1){
+                counter1++;
             }
+            else if(nums[i] == number2){
+                counter2++;
+            }
+            else if(counter1 == 0){
+                number1 = nums[i];
+                counter1=1;
+            }
+            else if(counter2 == 0 && nums[i] != number1){
+                number2 = nums[i];
+                counter2 = 1;
+            }
+
+
             else{
-                map.put(i,map.get(i)+1);
+                counter1--;
+                counter2--;
             }
         }
-        System.out.println(map);
-        for(int i : nums){
-            if(map.get(i)>limit && !list.contains(i)){
-                list.add(i);
+        //System.out.println(number1);
+        //System.out.println(number2);
+        int c1 = 0; int c2 = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == number1){
+                c1++;
             }
+            if(nums[i] == number2){
+                c2++;
+            }
+        }
+        if(c1>limit){
+            list.add(number1);
+        }
+        if(c2>limit && number2!=number1){
+            list.add(number2);
         }
         return list;
     }
+
 }
