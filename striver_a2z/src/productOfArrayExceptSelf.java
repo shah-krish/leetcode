3d01bd70f7509a4e3dd399a2add0b34a27746ba6
shah-krish <krishshah1411@gmail.com>
2025-05-23 17:11:05 -0400

    leetcode 238

--- striver_a2z/src/productOfArrayExceptSelf.java
+++ striver_a2z/src/productOfArrayExceptSelf.java
@@ -6,6 +6,27 @@ public class productOfArrayExceptSelf {
         int prefix = 1;
         int suffix = 1;
         int[] pre = new int[nums.length];
+        //int[] suf = new int[nums.length];
+        for(int i = 0; i<nums.length; i++){
+            pre[i] = prefix;
+            prefix *= nums[i];
+        }
+        for(int j = nums.length-1; j>=0; j--){
+            //suf[j] = suffix;
+            pre[j] = pre[j] * suffix;
+            suffix *= nums[j];
+        }
+        // for(int k = 0; k<nums.length; k++){
+        //     nums[k] = pre[k] * suf[k];
+        // }
+        return pre;
+    }
+}
+    /*
+    public int[] productExceptSelf(int[] nums) {
+        int prefix = 1;
+        int suffix = 1;
+        int[] pre = new int[nums.length];
         int[] suf = new int[nums.length];
         for(int i = 0; i<nums.length; i++){
             pre[i] = prefix;
@@ -21,3 +42,5 @@ public class productOfArrayExceptSelf {
         return nums;
     }
 }
+
+     */
