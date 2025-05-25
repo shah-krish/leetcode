a3ec3ac41793b0c92fa2e3c7b0b9cbc8e0a3dd54
shah-krish <krishshah1411@gmail.com>
2025-05-24 23:54:30 -0400

    leetcode 1

--- striver_a2z/src/twoSum.java
+++ striver_a2z/src/twoSum.java
@@ -5,18 +5,18 @@ public class twoSum {
     public static void main(String[] args) {
 
     }
+
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
         int[] ans = new int[2];
-        for(int i=0;i<nums.length;i++){
+        for(int i = 0; i < nums.length; i++){
             if(map.containsKey(target-nums[i])){
-                ans[0] = i;
-                ans[1] = map.get(target-nums[i]);
+                ans[0] = nums[i];
+                ans[1] = map.get(target-nums[i]);return ans;
             }
             else{
                 map.put(nums[i], i);
             }
-        }
-        return ans;
+        }return ans;
     }
 }
