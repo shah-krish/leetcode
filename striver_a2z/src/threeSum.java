ade659a934fe00cbf703878e94efc16c883770e9
shah-krish <krishshah1411@gmail.com>
2025-05-25 22:20:41 -0400

    leetcode 15

--- striver_a2z/src/threeSum.java
+++ striver_a2z/src/threeSum.java
@@ -1,48 +1,47 @@
+
 import java.util.*;
 public class threeSum {
     public static void main(String[] args) {
         int[] nums = {-2, 0, 1, 1, 2};
         System.out.println(threeSum(nums));
     }
-    //instead of using if ans.contains which has o(n) so it becomes n^3, we use loops to check for duplicates
     public static List<List<Integer>> threeSum(int[] nums) {
-        if (nums.length <= 2) {
-            return new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
+        //HashSet<List<Integer>> set = new HashSet<>();
+        if(nums.length<3){
+            return ans;
         }
         Arrays.sort(nums);
-        ArrayList<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            // Skip duplicate elements for 'i' to avoid duplicate triplets
-            if (i > 0 && nums[i] == nums[i - 1]) {
+        for(int i = 0; i<nums.length; i++){
+            if(i>0 && nums[i] == nums[i-1]){
                 continue;
             }
-            int j = i + 1;
-            int k = nums.length - 1;
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    List<Integer> list = new ArrayList<>();
+            int total = 0 - nums[i];
+            int low = i + 1;
+            int high = nums.length -1;
+            while(low<high){
+                if(nums[low] + nums[high] == total){
+                    ArrayList<Integer> list = new ArrayList<>();
                     list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[k]);
+                    list.add(nums[low]);
+                    list.add(nums[high]);
                     ans.add(list);
-                    // Skip duplicates for 'j'
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-                    // Skip duplicates for 'k'
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-                    j++;
-                    k--;
+
+                    while (low < high && nums[low] == nums[low + 1]) low++;
+                    while (low < high && nums[high] == nums[high - 1]) high--;
+
+                    low++;
+                    high--;
+                }
+                else if(nums[low] + nums[high] < total){
+                    low ++;
+                }
+                else{
+                    high --;
                 }
             }
         }
+        //ans.addAll(set);
         return ans;
     }
 }
