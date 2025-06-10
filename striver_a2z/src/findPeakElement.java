3948fed68f308cffe09b2ecb5ac0d3dd910b0ca6
shah-krish <krishshah1411@gmail.com>
2025-06-10 19:25:16 -0400

    leetcode  162

--- striver_a2z/src/findPeakElement.java
+++ striver_a2z/src/findPeakElement.java
@@ -3,15 +3,27 @@ public class findPeakElement {
 
     }
     public int findPeakElement(int[] nums) {
+        if(nums.length==1){
+            return 0;
+        }
+        if(nums[0]>nums[1]){
+            return 0;
+        }
+        if(nums[nums.length-1]>nums[nums.length-2]){
+            return nums.length-1;
+        }
         int low = 0;
         int high = nums.length-1;
         while(low<high){
             int mid = (low+high)/2;
-            if(nums[mid]>nums[mid+1]){
-                high = mid;
+            if(nums[mid+1]>nums[mid]){
+                low = mid + 1;
+            }
+            else if(nums[mid-1]>nums[mid]){
+                high = mid -1;
             }
             else{
-                low = mid+1;
+                return mid;
             }
         }
         return low;
