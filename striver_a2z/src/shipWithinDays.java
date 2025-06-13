56a4876015121d9c4e506f3b6028f104a5cfb267
shah-krish <krishshah1411@gmail.com>
2025-06-12 22:08:33 -0400

    leetcode  1011

--- striver_a2z/src/shipWithinDays.java
+++ striver_a2z/src/shipWithinDays.java
@@ -2,33 +2,36 @@ public class shipWithinDays {
     public static void main(String[] args) {
 
     }
-    public static int shipWithinDays(int[] weights, int days) {
-        int low = 1;
-        int high = Integer.MAX_VALUE-1;
-        while(low<=high){
+    public int shipWithinDays(int[] weights, int days){
+        int ans = 0;
+        if(days>weights.length){
+            return -1;
+        }
+        int high = 0, low = Integer.MIN_VALUE;
+        for(int i= 0; i< weights.length; i++){
+            high+=weights[i];
+            low = Math.max(low, weights[i]);
+        }
+        while(low<high){
             int mid = (low+high)/2;
-            int min = sum(weights,mid);
-            if(min>days){
-                low = mid + 1;
+            int temp = 0;
+            int counter = 1;
+            for(int i = 0; i<weights.length; i++){
+                if(temp+weights[i]>mid){
+                    counter++;
+                    temp = weights[i];
+                }
+                else{
+                    temp+=weights[i];
+                }
+            }
+            if(counter<=days){
+                high = mid;
             }
             else{
-                high = mid - 1;
+                low = mid+1;
             }
         }
         return low;
-    }
-    static int sum(int[] weights, int mid){
-        int currentWeight = 0;
-        int daysRequired = 1;
-
-        for (int weight : weights) {
-            if (currentWeight + weight > mid) {
-                daysRequired++;
-                currentWeight = weight;
-            } else {
-                currentWeight += weight;
-            }
-        }
-        return daysRequired;
     }
 }
