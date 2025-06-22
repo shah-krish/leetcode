9bbf83c9632ac221387b58084115c512df21023b
shah-krish <krishshah1411@gmail.com>
2025-06-21 20:03:31 -0400

    leetcode 1482

--- striver_a2z/src/minNoOfDaysToMakeMBouquets.java
+++ striver_a2z/src/minNoOfDaysToMakeMBouquets.java
@@ -2,9 +2,45 @@ import java.util.Arrays;
 
 public class minNoOfDaysToMakeMBouquets {
     public static void main(String[] args) {
-        int[] arr = {7,7,7,7,13,11,12,7};
-        System.out.println(minDays(arr,2,3));
+        int[] arr = {7, 7, 7, 7, 13, 11, 12, 7};
+        System.out.println(minDays(arr, 2, 3));
     }
+    public static int minDays(int[] bloomDay, int m, int k) {
+        if((long)m*k>bloomDay.length){
+            return -1;
+        }
+        int low = 1;
+        int high = 0;
+        for(int i = 0; i < bloomDay.length; i++){
+            high = Math.max(bloomDay[i], high);
+        }
+
+        while(low<high){
+            int mid = (low+high)/2;
+            int total = 0;
+            int flowerTracker = 0;
+            for(int i=0; i<bloomDay.length; i++){
+                if (bloomDay[i] <= mid) {
+                    flowerTracker++;
+                    if (flowerTracker == k) {
+                        total++;
+                        flowerTracker = 0;
+                    }
+                } else {
+                    flowerTracker = 0;
+                }
+            }
+            if(total>=m){
+                high = mid;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
+    /*
     public static int minDays(int[] bloomDay, int m, int k) {
         if ((long) m * k > bloomDay.length) {
             return -1;
@@ -40,3 +76,4 @@ public class minNoOfDaysToMakeMBouquets {
         return possible >=m;
     }
 }
+     */
