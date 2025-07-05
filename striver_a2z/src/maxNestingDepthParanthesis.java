3f82d64cab288c2ac1832e1b567903ef692f88c8
shah-krish <krishshah1411@gmail.com>
2025-07-04 20:00:01 -0400

    leetcode 1614

--- striver_a2z/src/maxNestingDepthParanthesis.java
+++ striver_a2z/src/maxNestingDepthParanthesis.java
@@ -2,18 +2,33 @@ public class maxNestingDepthParanthesis {
     public static void main(String[] args) {
 
     }
-    public static int maxDepth(String s) {
-        int max = 0;
+    public int maxDepth(String s) {
         int counter = 0;
-        for(int i =0; i<s.length(); i++){
+        int max = 0;
+        for(int i = 0; i<s.length(); i++){
             if(s.charAt(i)=='('){
                 counter++;
-                max = Math.max(counter, max);
+                max = Math.max(max, counter);
             }
             else if(s.charAt(i)==')'){
                 counter--;
             }
         }
         return max;
+
     }
+//    public static int maxDepth(String s) {
+//        int max = 0;
+//        int counter = 0;
+//        for(int i =0; i<s.length(); i++){
+//            if(s.charAt(i)=='('){
+//                counter++;
+//                max = Math.max(counter, max);
+//            }
+//            else if(s.charAt(i)==')'){
+//                counter--;
+//            }
+//        }
+//        return max;
+//    }
 }
