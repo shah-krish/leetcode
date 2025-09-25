1f63db485f5542b48f6e0bc08e57724d591426d4
shah-krish <krishshah1411@gmail.com>
2025-09-25 01:58:00 -0400

    maxProductSubarray

--- striver_a2z/src/maximumProductSubarray.java
+++ striver_a2z/src/maximumProductSubarray.java
@@ -1,22 +1,38 @@
 public class maximumProductSubarray {
     public static void main(String[] args) {
-
+        int[] nums = {2, -5, -2, -4, 3};
+        System.out.println(maxProduct(nums));
     }
-    public int maxProduct(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int prefix = 1, suffix = 1;
-        for(int i = 0 ; i<nums.length; i++){
-            if(prefix == 0 ){ //multiplication by 0, we don't need to carry it forward as it'll turn others 0
-                prefix = 1;
+    public static int maxProduct(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+        int curr = nums[0];
+        for(int i = 1; i<nums.length; i++){
+            if(nums[i] < 0 && min!=0){
+                min = min*nums[i];
+                max = Math.max(min,max);
             }
-            if(suffix == 0 ){
-                suffix = 1;
+            else if(nums[i] < 0 && min == 0){
+                min = nums[i];
+                max = Math.max(min,max);
             }
-
-            prefix = prefix * nums[i];
-            suffix = suffix * nums[nums.length-i-1];
-            max = Math.max(max, Math.max(suffix,prefix));
+            else if(nums[i] > 0 ){
+                max *= nums[i];
+            }
         }
         return max;
     }
+
 }
+/*
+        for(int i = 1; i<nums.length; i++){
+            counter *= nums[i];
+            max = Math.max(counter, max);
+
+            if(counter == 0 && nums[i] > 0){
+                counter = nums[i];
+            }
+            System.out.println(counter);
+        }
+        return max;
+ */
