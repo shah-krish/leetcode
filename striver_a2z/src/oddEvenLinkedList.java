966e964f2e79bde86fc9b3533e95302d0d1d77fa
shah-krish <krishshah1411@gmail.com>
2025-09-25 01:58:24 -0400

    oddEvenLinkedList

--- striver_a2z/src/oddEvenLinkedList.java
+++ striver_a2z/src/oddEvenLinkedList.java
@@ -25,7 +25,7 @@ public class oddEvenLinkedList {
         }
 
         public ListNode oddEvenList(ListNode head) {
-            if(head == null || head.next==null){
+            if(head==null || head.next == null){
                 return head;
             }
             ListNode curr = head;
@@ -34,37 +34,45 @@ public class oddEvenLinkedList {
                 counter++;
                 curr = curr.next;
             }
-            int[] even = new int[counter/2];
-            int[] odd = new int[counter - (counter/2)];
-
-            int i = 0, j = 0, k =1;
+            int line = Math.ceilDiv(counter,2);
+            int a = 1;
             ListNode c2 = head;
-            while(c2!=null){
-                if(k%2!=0){
-                    odd[i] = c2.val;
-                    i++;
-                }
-                else{
-                    even[j] = c2.val;
-                    j++;
-                }
-                k++;
+            while(a<=line+1){
                 c2 = c2.next;
+                a++;
             }
+            ListNode evenStart = c2;
+            ListNode oddStart = head.next;
+            int b = 0;
+            while(evenStart!=null){
+                if(b%2 ==0){
+                    int temp = evenStart.val;
+                    evenStart.val = oddStart.val;
+                    oddStart.val = temp;
 
-            ListNode c3 = head;
-            int p = 0, q = 0;
-            while(c3!=null){
-                if(p<odd.length){
-                    c3.val = odd[p];
-                    p++;
                 }
-                else{
-                    c3.val = even[q];
-                    q++;
-                }
-
-                c3 = c3.next;
+                evenStart = evenStart.next;
+                oddStart = oddStart.next;
+                b++;
+            }
+            ListNode oddSwap = head.next;
+            ListNode oddSwapNext = oddSwap.next;
+            ListNode oddEnd = c2;
+            while(oddSwapNext.next.next!=oddEnd){
+                int temp = oddSwap.val;
+                oddSwap.val = oddSwapNext.val;
+                oddSwapNext.val = temp;
+                oddSwap = oddSwap.next;
+                oddSwapNext = oddSwapNext.next;
+            }
+            ListNode evenSwap = c2.next;
+            ListNode evenSwapNext = c2.next.next;
+            while(evenSwapNext!=null){
+                int temp = evenSwap.val;
+                evenSwap.val = evenSwapNext.val;
+                evenSwapNext.val = temp;
+                evenSwap = evenSwap.next;
+                evenSwapNext = evenSwapNext.next;
             }
             return head;
         }
