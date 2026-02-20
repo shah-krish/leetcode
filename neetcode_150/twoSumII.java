482ba902c34027854a628a8d48b33dc3e121a3d2
shah-krish <krishshah1411@gmail.com>
2026-02-20 10:22:01 -0500

    feat: add solution for Two Sum II (LC #167) [O(n) Time / O(1) Space]

---
+++ neetcode_150/twoSumII.java
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+public class twoSumII {
+    public static void main(String[] args) {
+
+    }
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0;
+        int j = numbers.length-1;
+        int[] ans = new int[2];
+        while(i<j){
+            if(numbers[i]+numbers[j]==target){
+                ans[0] = i+1;
+                ans[1] = j+1;
+                return ans;
+            }
+            else if(numbers[i]+numbers[j]>target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return ans;
+    }
+}
+
