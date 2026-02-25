ae6b516c9cdbf31852bb5a419a563e7fa598769f
shah-krish <krishshah1411@gmail.com>
2026-02-25 18:58:48 -0500

    feat: add solution for Container With Most Water (LC #11) [O(n) Time / O(1) Space]

---
+++ neetcode_150/containsMostWater.java
@@ -0,0 +1,21 @@
+public class containsMostWater {
+    public static void main(String[] args) {
+
+    }
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length-1;
+        int counter = 0;
+        while(i<j && i<height.length-1 && j>=0){
+            int area = (j-i) * Math.min(height[i], height[j]);
+            counter = Math.max(counter,area);
+            if(height[j]>=height[i]){
+                i++;
+            }
+            else{
+                j--;
+            }
+
+        }
+        return counter;
+    }
+}
