444a50be8b1690aa569e4582103ae5c21b8ffc21
shah-krish <krishshah1411@gmail.com>
2026-03-07 17:16:09 -0500

    feat: add optimization for Longest Substring Without Repeating Characters (LC #3) [O(n) Time / O(n) Space]

--- neetcode_150/longestSubstringWithoutRepeatingCharacters.java
+++ neetcode_150/longestSubstringWithoutRepeatingCharacters.java
@@ -1,10 +1,31 @@
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
+import java.util.HashMap;
 import java.util.HashSet;
 
 public class longestSubstringWithoutRepeatingCharacters {
     public static void main(String[] args) {
 
     }
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length()<=1){
+            return s.length();
+        }
+        int answer = 0;
+        int left = 0, right = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        while(right<s.length()){
+            if(map.containsKey(s.charAt(right)) && map.get(s.charAt(right))>=left){
+               left = map.get(s.charAt(right))+1;
+            }
+            map.put(s.charAt(right), right);
+            answer = Math.max(right-left+1, answer);
+            right++;
+
+        }
+        return answer;
+    }
+}
+/* FIRST OWN SOLUTION
     public int lengthOfLongestSubstring(String s) {
         if(s.length()<=1){
             return s.length();
@@ -25,4 +46,4 @@ public class longestSubstringWithoutRepeatingCharacters {
         }
         return answer;
     }
-}
+ */
