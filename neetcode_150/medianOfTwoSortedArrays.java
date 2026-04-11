8a51ed56d7e4686931e8d999df0a1bd8760eb8e8
shah-krish <krishshah1411@gmail.com>
2026-04-11 15:50:15 -0400

    feat: add solution for Median of Two Sorted Arrays (LC #4) [O(n) Time / O(1) Space]

---
+++ neetcode_150/medianOfTwoSortedArrays.java
@@ -0,0 +1,31 @@
+//
+public class medianOfTwoSortedArrays {
+    public static void main(String[] args) {
+
+    }
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] nums3 = new int[nums1.length+nums2.length];
+        int i = 0, j = 0, k =0;
+        while (i < nums1.length && j < nums2.length) {
+            if(nums1[i] <= nums2[j]){
+                nums3[k++] = nums1[i++];
+            }
+            else{
+                nums3[k++] = nums2[j++];
+            }
+        }
+        while (i < nums1.length) {
+            nums3[k++] = nums1[i++];
+        }
+        while (j < nums2.length) {
+            nums3[k++] = nums2[j++];
+        }
+        int mid = nums3.length/2;
+        if(nums3.length%2==0){
+            return (nums3[mid - 1] + nums3[mid]) / 2.0;
+        }
+        else{
+            return nums3[mid];
+        }
+    }
+}
