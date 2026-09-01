f8b7ceb67da606f75da801af9860e70b99aa6de1
shah-krish <krishshah1411@gmail.com>
2026-09-01 15:16:03 +0530

    Re-solved Group Anagrams: practice and fluency check

--- neetcode_150/groupAnagrams.java
+++ neetcode_150/groupAnagrams.java
@@ -8,36 +8,24 @@ public class groupAnagrams {
     public static void main(String[] args) {
 
     }
-    public static List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> ans = new ArrayList<>();
-        if(strs.length==0){
-            return ans;
-        }
-        if(strs.length == 1){
-            List<String> l1 = new ArrayList<>();
-            l1.add(strs[0]);
-            ans.add(l1);
-            return ans;
-        }
+    public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String, List<String>> map = new HashMap<>();
-        for(int i = 0; i <strs.length; i++){
-            char[] word = strs[i].toCharArray();
-            Arrays.sort(word);
-            String sortedWord = new String(word);
-            //System.out.println(sortedWord);
-            if(map.containsKey(sortedWord)){
-                List<String> l1 = map.get(sortedWord);
-                l1.add(strs[i]);
-                map.put(sortedWord, l1);
+        for(int i = 0; i<strs.length; i++){
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            String sorted = new String(chars);
+            if(map.containsKey(sorted)){
+                map.get(sorted).add(strs[i]);
             }
-            else {
-                List<String> l1 = new ArrayList<>();
-                l1.add(strs[i]);
-                map.put(sortedWord, l1);
+            else{
+                List<String> a1 = new ArrayList<>();
+                a1.add(strs[i]);
+                map.put(sorted, a1);
             }
         }
-        for (List<String> group : map.values()) {
-            ans.add(group);
+        List<List<String>> ans = new ArrayList<>();
+        for(List value: map.values()){
+            ans.add(value);
         }
         return ans;
     }
